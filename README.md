29f35fd39ac03727d270661fb031d1915070e154
Gavin <gavinhaynes32@gmail.com>
2024-07-12 13:55:31 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Breakemo
+ Swamp
---
+++ src/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+# breakemo 
+# Gavin Haynes
+
+import os
+import random
+import pygame
+
+def main():
+    pygame.init()
+    
+if __name__ == "__main__":
+    main()
+    pygame.quit()
